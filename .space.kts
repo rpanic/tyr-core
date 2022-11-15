3c454c3d87a9b149a4dd0db660bbcbb94de04154
Raphael Panic <raphael.panic@gmx.at>
2022-11-15 15:17:50 +0100

    Updated .space.kts

--- .space.kts
+++ .space.kts
@@ -1,19 +1,10 @@
 job("build") {
     host("Build and push docker image") {
-        dockerBuildPush {
-            context = "docker"
-            // path to Dockerfile relative to the project root
-            // if 'file' is not specified, Docker will look for it in 'context'/Dockerfile
-            file = "Dockerfile"
-            // to add a raw list of additional build arguments, use
-            // extraArgsForBuildCommand = listOf("...")
-            // to add a raw list of additional push arguments, use
-            // extraArgsForPushCommand = listOf("...")
-            // image tags
-            tags {
-                // use current job run number as a tag - '0.0.run_number' ${"$"}JB_SPACE_EXECUTION_NUMBER
-                +"space.rpanic.com/rpanic/tyr-core:1.0.${"$"}JB_SPACE_GIT_REVISION-${"$"}JB_SPACE_EXECUTION_NUMBER"
-            }
+        shellScript {
+            content = """
+                docker build -t space.rpanic.com:8390/tyr-core:1.0.${'$'}JB_SPACE_GIT_REVISION .
+                docker push space.rpanic.com:8390/tyr-core:1.0.${'$'}JB_SPACE_GIT_REVISION
+            """.trimIndent()
         }
     }
 }

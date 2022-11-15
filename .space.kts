0f82e1456dbd6298e648d843f7eddfcd52e47358
Raphael Panic <raphael.panic@gmx.at>
2022-11-15 19:01:30 +0100

    Updated .space.kts

--- .space.kts
+++ .space.kts
@@ -2,7 +2,7 @@ job("build") {
     host("Build and push docker image") {
         shellScript {
             content = """
-                docker login registry.space.rpanic.com -u rpanic -p ${'$'}REGISTRY_PW
+                docker login registry.space.rpanic.com -u rpanic -p ${'$'}registry_pw
                 docker build -t registry.space.rpanic.com/p/tyr/containers/tyr-core:${'$'}JB_SPACE_GIT_REVISION .
                 docker push registry.space.rpanic.com/p/tyr/containers/tyr-core:${'$'}JB_SPACE_GIT_REVISION
             """

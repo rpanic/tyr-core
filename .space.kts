03bef5c3f3b1302417019923d3dca55de23c0dec
Raphael Panic <raphael.panic@gmx.at>
2022-11-15 18:48:42 +0100

    Updated .space.kts

--- .space.kts
+++ .space.kts
@@ -2,9 +2,10 @@ job("build") {
     host("Build and push docker image") {
         shellScript {
             content = """
-                docker build -t registry.space.rpanic.com/tyr-core:1.0.${'$'}JB_SPACE_GIT_REVISION .
+                docker build -t registry.space.rpanic.com/p/tyr/containers/tyr-core:1.0.${'$'}JB_SPACE_GIT_REVISION .
+                docker push registry.space.rpanic.com/p/tyr/containers/tyr-core:1.0.${'$'}JB_SPACE_GIT_REVISION
             """
-            //docker push registry.space.rpanic.com/tyr-core:1.0.${'$'}JB_SPACE_GIT_REVISION
+            //
         }
     }
 }

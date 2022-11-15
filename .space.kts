bdf8eed9ce70ef95b8cb74d8713445c3976deeeb
Raphael Panic <raphael.panic@gmx.at>
2022-11-15 19:50:10 +0100

    Fixed typo

--- .space.kts
+++ .space.kts
@@ -2,7 +2,7 @@ job("build") {
     host("Build and push docker image") {
         env["PW"] = Secrets("registry-pw")
         shellScript {
-            val pw = System.getEnv("PW")
+            val pw = System.getenv("PW")
             content = """
                 docker login registry.space.rpanic.com -u rpanic -p $pw
                 docker build -t registry.space.rpanic.com/p/tyr/containers/tyr-core:${'$'}JB_SPACE_GIT_REVISION .

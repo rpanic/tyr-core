c0e021a0ec3ac3e71be63632ba0eff9f5dc6156d
Raphael Panic <raphael.panic@gmx.at>
2022-11-15 19:47:13 +0100

    Updated .space.kts

--- .space.kts
+++ .space.kts
@@ -1,8 +1,10 @@
 job("build") {
     host("Build and push docker image") {
+        env["PW"] = Secrets("registry-pw")
         shellScript {
+            val pw = System.getEnv("PW")
             content = """
-                docker login registry.space.rpanic.com -u rpanic -p ${'$'}registry_pw
+                docker login registry.space.rpanic.com -u rpanic -p $pw
                 docker build -t registry.space.rpanic.com/p/tyr/containers/tyr-core:${'$'}JB_SPACE_GIT_REVISION .
                 docker push registry.space.rpanic.com/p/tyr/containers/tyr-core:${'$'}JB_SPACE_GIT_REVISION
             """
